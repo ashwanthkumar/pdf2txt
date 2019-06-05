38c258248a41d2f93b13610ce443a34fdecb06ce
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-06-05 10:06:43 +0530

    We now handle 4 column multi-page layouts too

--- src/test/scala/in/ashwanthkumar/pdf2txt/TextStripperWithPositionsTest.scala
+++ src/test/scala/in/ashwanthkumar/pdf2txt/TextStripperWithPositionsTest.scala
@@ -6,9 +6,17 @@ import org.scalatest.FlatSpec
 import scala.collection.JavaConverters._
 
 class TextStripperWithPositionsTest extends FlatSpec {
-  "TextStripperWithPositions" should "process multi-column layout" in {
+  "TextStripperWithPositions" should "process 3 colum layout without any lines separating them" in {
     val pdfDocument =
       TextStripperWithPositions.readFile(getClass.getResourceAsStream("/pdfs/3_column_layout_with_header_footer.pdf"))
+    pdf2txt(pdfDocument)
+  }
+
+  it should "process 4 column layout with lines separating them" in {
+    val pdfDocument =
+      TextStripperWithPositions.readFile(
+        getClass.getResourceAsStream("/pdfs/4_column_layout_with_markers_header_footer.pdf")
+      )
     pdf2txt(pdfDocument)
   }
 
